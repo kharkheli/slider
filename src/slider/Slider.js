0b6b403017506e4cf2b6d9e512f1b0612cb72fc8
khaekheli <iliakharkheli6@gmail.com>
2022-12-18 12:11:49 +0400

    add api fetch

--- src/slider/Slider.js
+++ src/slider/Slider.js
@@ -5,36 +5,59 @@ export default function Slider() {
   const videoRef = useRef(null);
   const [current, setCurrent] = useState(null);
   const [slides, setSlides] = useState([
-    {
-      type: "video",
-      path: "https://www.w3schools.com/html/mov_bbb.mp4",
-      play_seconds: 10,
-    },
-    {
-      type: "photo",
-      path: "https://i.natgeofe.com/n/c9107b46-78b1-4394-988d-53927646c72b/1095.jpg",
-      play_seconds: 5,
-    },
-    {
-      type: "photo",
-      path: "https://cdn.britannica.com/67/19367-050-885866B4/Valley-Taurus-Mountains-Turkey.jpg",
-      play_seconds: 5,
-    },
-    {
-      type: "photo",
-      path: "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg",
-      play_seconds: 5,
-    },
-    {
-      type: "video",
-      path: "/video1.mp4",
-      play_seconds: 10,
-    },
+    // {
+    //   type: "video",
+    //   filepath: "https://www.w3schools.com/html/mov_bbb.mp4",
+    //   seconds: 10,
+    // },
+    // {
+    //   type: "photo",
+    //   filepath: "https://i.natgeofe.com/n/c9107b46-78b1-4394-988d-53927646c72b/1095.jpg",
+    //   seconds: 5,
+    // },
+    // {
+    //   type: "photo",
+    //   filepath: "https://cdn.britannica.com/67/19367-050-885866B4/Valley-Taurus-Mountains-Turkey.jpg",
+    //   seconds: 5,
+    // },
+    // {
+    //   type: "photo",
+    //   filepath: "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg",
+    //   seconds: 5,
+    // },
+    // {
+    //   type: "video",
+    //   filepath: "/video1.mp4",
+    //   seconds: 10,
+    // },
   ]);
 
   const nextSlide = useMemo(() => {
     return (current + 1) % slides.length;
   }, [current]);
+
+  useEffect(() => {
+    const href = window.location.href;
+    const id = href
+      .split("?")
+      .find((item) => item.includes("id="))
+      .split("=")[1];
+    console.log(id);
+    fetch(`http://mzekabani.ge/mzekabani.ge/slider/api/api.php?id=${id}`)
+      .then((res) => res.json())
+      .then((res) =>
+        setSlides(
+          res.data.map((item) => {
+            if (item.filepath.includes("mp4"))
+              return {
+                ...item,
+                type: "video",
+              };
+            return item;
+          })
+        )
+      );
+  }, []);
 
   const previousSlide = useMemo(() => {
     if (current === null) return null;
@@ -52,7 +75,7 @@ export default function Slider() {
 
     const interval = setTimeout(() => {
       setCurrent((prev) => (prev + 1) % slides.length);
-    }, slides[current || 0].play_seconds * 1000 + 1000);
+    }, slides[current || 0].seconds * 1000 + 1000);
     return () => {
       clearTimeout(interval);
       clearTimeout(videoTimer);
@@ -63,7 +86,7 @@ export default function Slider() {
       {slides.map((slide, index) => {
         return (
           <div
-            key={slide.path}
+            key={slide.filepath}
             className={`${style.slide} ${
               current === index
                 ? style.active
@@ -81,10 +104,10 @@ export default function Slider() {
                 ref={(current || 0) === index ? videoRef : null}
                 className={style.slide_content}
               >
-                <source src={slide.path} />
+                <source src={slide.filepath} />
               </video>
             ) : (
-              <img className={style.slide_content} src={slide.path} />
+              <img className={style.slide_content} src={slide.filepath} />
             )}
           </div>
         );
